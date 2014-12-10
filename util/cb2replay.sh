97721fb5275dac95a2c514998a5935164bd286cc
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2014-12-10 15:09:17 -0600

    util/cb2replay.sh: Add C header and output compileable file
    With this change, we define the helper functions that we make up, and
    generate a C file that can be added directly to the coreboot tree.
    
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- util/cb2replay.sh
+++ util/cb2replay.sh
@@ -2,6 +2,44 @@ iport=0x2000
 dport=0x2004
 stsport=0x204c
 
+header="
+#include <arch/io.h>
+
+static void sync_read(void)
+{
+	inb(0x03c3);
+	inl($stsport);
+}
+
+static uint32_t radeon_read(uint32_t reg_addr)
+{
+	outl(reg_addr, $iport);
+	return inl($dport);
+}
+
+static void radeon_write(uint32_t reg_addr, uint32_t value)
+{
+	outl(reg_addr, $iport);
+	outl(value, $dport);
+}
+
+static uint32_t radeon_read_sync(uint32_t reg_addr)
+{
+	sync_read();
+	return radeon_read(reg_addr);
+}
+
+static void radeon_write_sync(uint32_t reg_addr, uint32_t value)
+{
+	sync_read();
+	radeon_write(reg_addr, value);
+}
+
+void run_replay(void)
+{"
+
+echo "$header"
+
 egrep "c000:[0-9,a-f]{4}" $1 |
 grep -v "Running option rom at c000:0003" |
 sed "s/c000\:[0-9,a-f]\{4\}\ /\t/g" |
@@ -19,3 +57,5 @@ sed "s/sync_read();[^\r]*\r\tradeon_write(/radeon_write_sync(/g" |
 sed "s/sync_read();[^\r]*\r\tradeon_read(/radeon_read_sync(/g" |
 
 tr '\r' '\n'
+
+echo "}"
