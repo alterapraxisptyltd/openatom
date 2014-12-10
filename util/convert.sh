5906b3839b293c79dbe7ce85ada5c75635c67609
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2014-12-09 22:55:54 -0600

    util/convert.sh: Convert register-CLEAR opcodes to C code
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- util/convert.sh
+++ util/convert.sh
@@ -17,4 +17,9 @@ sed "s/.*0725.*AND.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*\([0-9,a-f]*\)/\tmy_radeon_an
 sed "s/.*0765.*AND.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*\([0-9,a-f]*\)/\tmy_radeon_and8\(0x\1, BYTE_1, 0x\2\);/g" |
 sed "s/.*07a5.*AND.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*\([0-9,a-f]*\)/\tmy_radeon_and8\(0x\1, BYTE_2, 0x\2\);/g" |
 sed "s/.*07e5.*AND.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*\([0-9,a-f]*\)/\tmy_radeon_and8\(0x\1, BYTE_3, 0x\2\);/g" |
-sed "s/.*070d.*AND.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*\([0-9,a-f]*\)/\tmy_radeon_and16\(0x\1, BYTES_01, 0x\2\);/g"
+sed "s/.*070d.*AND.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*\([0-9,a-f]*\)/\tmy_radeon_and16\(0x\1, BYTES_01, 0x\2\);/g" |
+sed "s/.*5420.*CLEAR.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*/\tmy_radeon_write8\(0x\1, BYTE_0, 0);/g" |
+sed "s/.*5428.*CLEAR.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*/\tmy_radeon_write8\(0x\1, BYTE_1, 0);/g" |
+sed "s/.*5430.*CLEAR.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*/\tmy_radeon_write8\(0x\1, BYTE_2, 0);/g" |
+sed "s/.*5408.*CLEAR.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*/\tmy_radeon_write16\(0x\1, BYTES_01, 0);/g" |
+sed "s/.*5400.*CLEAR.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*/\tmy_radeon_write32\(0x\1, 0);/g"
