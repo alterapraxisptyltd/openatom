6a94d49dc0b221527232b49260e39378c9757692
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2014-12-10 14:32:55 -0600

    util/cb2replay.sh: Make VGA ports configurable
    The location of the VGA IO ports depends on how coreboot sets up the
    PCI BARs. Allow those ports to be changed by placing them in variables
    rather than hardcoding in sed strings
    
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- util/cb2replay.sh
+++ util/cb2replay.sh
@@ -1,3 +1,7 @@
+iport=0x2000
+dport=0x2004
+stsport=0x204c
+
 egrep "c000:[0-9,a-f]{4}" $1 |
 grep -v "Running option rom at c000:0003" |
 sed "s/c000\:[0-9,a-f]\{4\}\ /\t/g" |
@@ -6,10 +10,10 @@ sed "s/=\ 0x\([0-9,a-f]*\)/\/\*\ \1\ \*\//g" |
 
 tr '\n' '\r' |
 
-sed "s/outl(0x0\{0,4\}\([0-9,a-f]*\), 0x2000);[^\r]*\r\toutl(0x\([0-9,a-f]*\), 0x2004);/radeon_write(0x\1, 0x\2);/g" |
-sed "s/outl(0x0\{0,4\}\([0-9,a-f]*\), 0x2000);[^\r]*\r\tinl(0x2004);/radeon_read(0x\1);/g" |
+sed "s/outl(0x0\{0,4\}\([0-9,a-f]*\), $iport);[^\r]*\r\toutl(0x\([0-9,a-f]*\), $dport);/radeon_write(0x\1, 0x\2);/g" |
+sed "s/outl(0x0\{0,4\}\([0-9,a-f]*\), $iport);[^\r]*\r\tinl($dport);/radeon_read(0x\1);/g" |
 
-sed "s/inb(0x03c3);[^\r]*\r\tinl(0x204c);[^\r]*/sync_read();/g" |
+sed "s/inb(0x03c3);[^\r]*\r\tinl($stsport);[^\r]*/sync_read();/g" |
 
 sed "s/sync_read();[^\r]*\r\tradeon_write(/radeon_write_sync(/g" |
 sed "s/sync_read();[^\r]*\r\tradeon_read(/radeon_read_sync(/g" |
