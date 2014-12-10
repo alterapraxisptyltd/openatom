82bc7756b6c6d992de957f9de27aac8c1f4db346
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2014-12-09 22:04:49 -0600

    util/convert.sh: Convert EOT entries to C function terminators
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- util/convert.sh
+++ util/convert.sh
@@ -1,3 +1,4 @@
 sed "s/.*\0105.*MOVE.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*\([0-9,a-f]*\)/\tmy_radeon_write32\(0x\1, 0x\2\);/g" $1 |
 sed "s/.*52.*CALL_TABLE.*(\([0-9,a-z,_,/,A-Z]*\))/\tmy_exec_table(\1);/g" |
-sed "s/.*51.*DELAY_MicroSec[^0-9,a-f]*\([0-9,a-f]*\)/\tmy_udelay(0x\1);/g"
+sed "s/.*51.*DELAY_MicroSec[^0-9,a-f]*\([0-9,a-f]*\)/\tmy_udelay(0x\1);/g" |
+sed "s/.*5b.*EOT.*/\treturn;\n}/g"
