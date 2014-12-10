7a3d1b749450942941e39e8caeae07b02de6482f
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2014-12-09 22:23:07 -0600

    util/convert.sh: Convert register-OR opcodes to C code
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- util/convert.sh
+++ util/convert.sh
@@ -1,4 +1,10 @@
 sed "s/.*\0105.*MOVE.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*\([0-9,a-f]*\)/\tmy_radeon_write32\(0x\1, 0x\2\);/g" $1 |
 sed "s/.*52.*CALL_TABLE.*(\([0-9,a-z,_,/,A-Z]*\))/\tmy_exec_table(\1);/g" |
 sed "s/.*51.*DELAY_MicroSec[^0-9,a-f]*\([0-9,a-f]*\)/\tmy_udelay(0x\1);/g" |
-sed "s/.*5b.*EOT.*/\treturn;\n}/g"
+sed "s/.*5b.*EOT.*/\treturn;\n}/g" |
+sed "s/.*0d25.*OR.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*\([0-9,a-f]*\)/\tmy_radeon_or8\(0x\1, BYTE_0, 0x\2\);/g" |
+sed "s/.*0d65.*OR.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*\([0-9,a-f]*\)/\tmy_radeon_or8\(0x\1, BYTE_1, 0x\2\);/g" |
+sed "s/.*0da5.*OR.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*\([0-9,a-f]*\)/\tmy_radeon_or8\(0x\1, BYTE_2, 0x\2\);/g" |
+sed "s/.*0de5.*OR.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*\([0-9,a-f]*\)/\tmy_radeon_or8\(0x\1, BYTE_3, 0x\2\);/g" |
+sed "s/.*0d0d.*OR.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*\([0-9,a-f]*\)/\tmy_radeon_or16\(0x\1, BYTES_01, 0x\2\);/g" |
+sed "s/.*0d05.*OR.*reg\[\([0-9,a-f]*\)][^0-9,a-f]*\([0-9,a-f]*\)/\tmy_radeon_or32\(0x\1, 0x\2\);/g"
