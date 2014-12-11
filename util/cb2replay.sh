2a20b98ee6e4168347b07eaffbde4bf3fff6cd14
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2014-12-10 19:56:17 -0600

    util/cb2replay: Decode known VGA IO accessses
    Coreboot has functions for certain VGA IO access patterns. If we
    detect such a pattern, convert it to a coreboot call.
    
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- util/cb2replay.sh
+++ util/cb2replay.sh
@@ -4,10 +4,11 @@ stsport=0x204c
 
 header="
 #include <arch/io.h>
+#include <pc80/vga_io.h>
 
 static void sync_read(void)
 {
-	inb(0x03c3);
+	vga_enable_read();
 	inl($stsport);
 }
 
@@ -58,9 +59,14 @@ sed "s/inb(0x03c3);[^\r]*\r\tinl($stsport);[^\r]*/sync_read();/g" |
 sed "s/sync_read();[^\r]*\r\tradeon_write(/radeon_write_sync(/g" |
 sed "s/sync_read();[^\r]*\r\tradeon_read(/radeon_read_sync(/g" |
 
+sed "s/outb(0x\([0-9,a-f]*\), 0x03c2);/vga_misc_write(0x\1);/g" |
+sed "s/inb(0x03cc);/vga_misc_read();/g" |
+sed "s/inb(0x03c3));/vga_enable_read();/g" |
+sed "s/outw(0x\([0-9,a-f]\{2\}\)\([0-9,a-f]\{2\}\), 0x03c4);/outw(0x\1\2, 0x03c4);\/\*\ vga_sr_write(0x\2, 0x\1); \*\//g" |
+sed "s/outb(0x\([0-9,a-f]*\), 0x03c4);[^\r]*\r\tinb(0x03c5);/vga_sr_read(0x\1);/g" |
+sed "s/outb(0x\([0-9,a-f]*\), 0x03ce);[^\r]*\r\toutb(0x\([0-9,a-f]*\), 0x03cf);/vga_gr_write(0x\1, 0x\2);/g" |
+
 sed "/\r/s/halt_sys: in x86emuOp_halt/}\r/g" |
-
 sed "/\r/s/[^\r]*runInt[0-9,a-f]*();: starting execution of INT\([0-9,a-f]*\)[^\r]*/void replay_int\1(void)\r{/g" |
-
 
 tr '\r' '\n'
