be1ef6e6c27331ca104ad5f2d130d9dec7b69952
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2014-12-15 21:27:16 -0600

    radeon_replay: Provide register access helpers
    These helpers take the register number, rather than the register
    address as a parameter, and thus match the representation in the atom
    tables. Since they use the radeon_[read/write]_sync internally, they
    provide identical trace output.
    
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- radeon_replay/radeon_util.h
+++ radeon_replay/radeon_util.h
@@ -11,6 +11,16 @@ void radeon_write_sync(uint32_t reg_addr, uint32_t value);
 void radeon_delay(uint32_t internal_timer);
 void radeon_udelay(uint32_t usec);
 
+static uint32_t radeon_reg_read(uint32_t reg_addr)
+{
+	return radeon_read_sync(reg_addr << 2);
+}
+
+static void radeon_reg_write(uint32_t reg_addr, uint32_t value)
+{
+	radeon_write_sync(reg_addr << 2, value);
+}
+
 void run_radeon_tests(void);
 
 #endif /* RADEON_UTIL */
