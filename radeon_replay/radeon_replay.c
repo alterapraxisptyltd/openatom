d238d6932f876c4e6503a118966178dbc32d5ee7
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2014-12-18 11:36:07 -0600

    radeon_replay: Read DisplayPort configuration data on AUX 0
    This uses AUX channel 0 to read the first 356 bytes of the DisplayPort
    configuration data (DPCD), and dump the contents.
    
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- radeon_replay/radeon_replay.c
+++ radeon_replay/radeon_replay.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <sys/io.h>
 
+#include "radeon_init_native.h"
 #include "replay.h"
 
 static void dump_array(const uint8_t *what, size_t len)
@@ -17,12 +18,23 @@ static void dump_array(const uint8_t *what, size_t len)
 
 int main(void)
 {
+	int ret;
+	uint8_t dpcd[256];
 	printf("radeon_replay AtomBIOS replayer\n");
 
 	if (ioperm(0x2000, 0x2000, 1) || ioperm(0x300, 0x100, 1)) {
 		printf("No IO permissions. Are we root?\n");
 		return;
 	}
+
+	printf("Let's try to read from the aux channel\n");
+
+	ret = radeon_read_dpcd(0, dpcd, 0, sizeof(dpcd));
+	if (ret < 0) {
+		printf("Something bad happened\n");
+		return 1;
+	}
+	dump_array(dpcd, sizeof(dpcd));
 
 	printf("Replaying initial init\n");
 	run_replay();
