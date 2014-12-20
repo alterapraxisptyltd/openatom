df1cfd776a2edbe3169d699bcc05820fe087ee32
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2014-12-19 18:11:41 -0600

    radeon_replay: Decode EDID of devices behind AUX 0
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- radeon_replay/radeon_replay.c
+++ radeon_replay/radeon_replay.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <sys/io.h>
 
+#include "edid.h"
 #include "radeon_init_native.h"
 #include "replay.h"
 
@@ -19,7 +20,8 @@ static void dump_array(const uint8_t *what, size_t len)
 int main(void)
 {
 	int dpcd_ret, edid_ret;
-	uint8_t dpcd[256], edid[256];
+	uint8_t dpcd[256], edid_raw[256];
+	struct edid edid;
 	printf("radeon_replay AtomBIOS replayer\n");
 
 	if (ioperm(0x2000, 0x2000, 1) || ioperm(0x300, 0x100, 1)) {
@@ -31,7 +33,7 @@ int main(void)
 	dpcd_ret = radeon_read_dpcd(0, dpcd, 0, sizeof(dpcd));
 
 	printf("Now let's try to get an EDID\n");
-	edid_ret = radeon_read_dp_aux_i2c(0, 0x50, edid, 0, sizeof(edid));
+	edid_ret = radeon_read_dp_aux_i2c(0, 0x50, edid_raw, 0, sizeof(edid_raw));
 
 	if (dpcd_ret >= 0) {
 		printf("Here's the DPCD data:\n");
@@ -39,8 +41,10 @@ int main(void)
 	}
 	if (edid_ret >= 0) {
 		printf("And here's the EDID:\n");
-		dump_array(edid, sizeof(edid));
+		dump_array(edid_raw, sizeof(edid_raw));
 	}
+
+	decode_edid(edid_raw, sizeof(edid_raw), &edid);
 
 	printf("Replaying initial init\n");
 	run_replay();
