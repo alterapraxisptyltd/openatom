4f0e0a225aa5fb11e5084c356f5e365a1b412c90
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2014-12-29 20:33:03 -0600

    radeon_replay: Use getopt() to control what we want to do
    As more and more branches popped up, each trying to do something else,
    a lot of "#if 0" and similar abominations started appearing. They were
    never merged into the main branch, but they did show the need to be
    able to select which action or action to perform.
    
    As such, introduce getopt() handling. We support four actions already.
    
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

--- radeon_replay/radeon_replay.c
+++ radeon_replay/radeon_replay.c
@@ -1,10 +1,22 @@
+#include <getopt.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <sys/io.h>
 
 #include "edid.h"
 #include "radeon_init_native.h"
 #include "replay.h"
+
+struct global_cfg {
+	bool need_io_perm;
+	bool run_replay;
+	bool run_tests;
+	bool read_dcpd;
+	bool read_edid;
+};
 
 static void dump_array(const uint8_t *what, size_t len)
 {
@@ -17,48 +29,107 @@ static void dump_array(const uint8_t *what, size_t len)
 	printf("\n");
 }
 
-int main(void)
+static void parse_options(int argc, char *argv[], struct global_cfg *config)
 {
+	int opt, option_index = 0;
+	struct option long_options[] = {
+		{"replay",	no_argument,		0, 'r'},
+		{"tests",	no_argument,		0, 't'},
+		{"dpcd",	no_argument,		0, 'd'},
+		{"edid",	no_argument,		0, 'e'},
+		{0, 0, 0, 0}
+	};
+
+	memset(config, 0, sizeof(*config));
+
+	/*
+	 * Parse arguments
+	 */
+	while (1) {
+		opt = getopt_long(argc, argv, "rtde",
+				  long_options, &option_index);
+
+		if (opt == EOF)
+			break;
+
+		switch (opt) {
+		case 'r':
+			config->need_io_perm = true;
+			config->run_replay = true;
+			break;
+		case 't':
+			config->need_io_perm = true;
+			config->run_tests = true;
+			break;
+		case 'd':
+			config->need_io_perm = true;
+			config->read_dcpd = true;
+			break;
+		case 'e':
+			config->need_io_perm = true;
+			config->read_edid = true;
+			break;
+		default:
+			/* Invalid option. getopt will have printed something */
+			exit(EXIT_FAILURE);
+			break;
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+
 	int dpcd_ret, edid_ret;
 	uint8_t dpcd[256], edid_raw[256];
 	struct edid edid;
+	struct global_cfg config;
 	printf("radeon_replay AtomBIOS replayer\n");
 
-	if (ioperm(0x2000, 0x2000, 1) || ioperm(0x300, 0x100, 1)) {
-		printf("No IO permissions. Are we root?\n");
-		return 1;
+	parse_options(argc, argv, &config);
+
+	if (config.need_io_perm) {
+		if (ioperm(0x2000, 0x2000, 1) || ioperm(0x300, 0x100, 1)) {
+			printf("No IO permissions. Are we root?\n");
+			return EXIT_FAILURE;
+		}
 	}
 
-	printf("Let's try to read from the aux channel\n");
-	dpcd_ret = radeon_read_dpcd(0, dpcd, 0, sizeof(dpcd));
+	if (config.read_dcpd) {
+		printf("Let's try to read from the aux channel\n");
+		dpcd_ret = radeon_read_dpcd(0, dpcd, 0, sizeof(dpcd));
+	}
 
-	printf("Now let's try to get an EDID\n");
-	edid_ret = radeon_read_dp_aux_i2c(0, 0x50, edid_raw, 0, sizeof(edid_raw));
+	if (config.read_edid) {
+		printf("Now let's try to get an EDID\n");
+		edid_ret = radeon_read_dp_aux_i2c(0, 0x50, edid_raw, 0, sizeof(edid_raw));
+	}
 
-	if (dpcd_ret >= 0) {
+	if (config.read_dcpd && (dpcd_ret >= 0)) {
 		printf("Here's the DPCD data:\n");
 		dump_array(dpcd, sizeof(dpcd));
 	}
-	if (edid_ret >= 0) {
+	if (config.read_edid && (edid_ret >= 0)) {
 		printf("And here's the EDID:\n");
 		dump_array(edid_raw, sizeof(edid_raw));
+		decode_edid(edid_raw, sizeof(edid_raw), &edid);
 	}
 
-	decode_edid(edid_raw, sizeof(edid_raw), &edid);
 
-	printf("Replaying initial init\n");
-	run_replay();
+	if (config.run_replay) {
+		printf("Replaying initial init\n");
+		run_replay();
 
-	printf("Replaying first int10 call\n");
-	replay_int10_c1();
+		printf("Replaying first int10 call\n");
+		replay_int10_c1();
 
-	printf("Replaying second int10 call\n");
-	replay_int10_c2();
+		printf("Replaying second int10 call\n");
+		replay_int10_c2();
 
-	printf("Replaying third int10 call\n");
-	replay_int10_c3();
+		printf("Replaying third int10 call\n");
+		replay_int10_c3();
 
-	printf("Did it work ?\n");
-
-	return 0;
+		printf("Did it work ?\n");
+	}
+	return EXIT_SUCCESS;
 }
