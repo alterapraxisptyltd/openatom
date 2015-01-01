f039481c16fdc8d1d59e1b227bfd76a965651b18
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2014-12-31 22:25:32 -0600

    util: Add script to decode AUX transactions
    This regex-based script simplifies the IO trace significantly around
    AUX channel transactions. The code produced is not compileable, but
    meant for diffing. That's also the reason this script is separate, and
    not part of cb2replay.sh
    
    Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

---
+++ util/aux2c.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Decode AUX channel transactions from a replay log
+# This works for channel 0 FIFO only
+#
+
+hex=0-9,a-f
+aux_ctl=0x6200
+fifo_ctl=0x6204
+aux_ctl2=0x620c
+status=0x6210
+fifo=0x6218
+pad_ctl=0x6530
+
+# We don't care if it's a _sync read or not
+#sed '/radeon_read/,/radeon_write/{s/_sync/_fuck/g}' $1 |
+sed -r '/radeon_(read|write)/s/_sync//g' $1 |
+# Prune scratch registers accesses (usually at 0x9fxxxxxx)
+egrep -v "radeon_(read|write)(_sync|)\(0x9f[$hex]{6}" |
+
+# Prune reads from FIFO control registers
+sed -r "/radeon_read\($fifo_ctl\);/d" |
+
+# Kill accesses to pad control register; They're uninteresting
+sed -r "/radeon_(read|write)\($pad_ctl/d" |
+
+# Accesses to there registers tell us almost nothing interesting
+sed -r "/radeon_(read|write)\($aux_ctl/d" |
+sed -r "/radeon_(read|write)\($aux_ctl2/d" |
+
+# This IO indicates we're about to read the FIFO
+sed -r "s/radeon_write\($fifo, 0x80000001/fifo_switch\(/g" |
+# FIFO writes are fairly straightforward
+sed -r "s/radeon_write\($fifo, 0x0000([$hex]{2})00/fifo_write\(0x\1/g" |
+# Now the same for FIFO reads
+sed -r "s/radeon_read\($fifo\); \/\* 00[$hex]{2}([$hex]{2})01/fifo_read\(\); \/\* \1/g" |
+# Detect where we start seeding the FIFO
+sed -r "s/radeon_write\($fifo, 0x8000([$hex])[$hex]00/fifo_start\(0x\1/g" |
+
+# Make reads to status register more obvious
+sed -r "s/radeon_read\($status/get_status\(/g" |
+
+tr '\n' '\r' |
+
+# Now extract the transaction size on succesive writes to to FIFO control
+sed -r "s/radeon_write\($fifo_ctl, 0x[$hex]{8}\);\r\tradeon_write\($fifo_ctl, 0x[$hex]{2}([$hex]{2})[$hex]{4}/tx_size\(0x\1/g" |
+# Extract address from subsequent FIFO writes
+sed -r "/fifo_start\(/s/0x([$hex])\);\r\tfifo_write\(0x([$hex]{2})\);\r\tfifo_write\(0x([$hex]{2})/0x\1, 0x\2\3/g" |
+# Include the tx size in the argument list of fifo_start
+sed -r "s/tx_size\((0x[$hex]{2})\);\r\tfifo_start\((0x[$hex]), (0x[$hex]*)/fifo_start\(\2, \1, \3/g" |
+# Explicitate each transactions
+sed -r "s/fifo_start\(0x9, /aux_read\(/g" |
+sed -r "s/fifo_start\(0x8, /aux_write\(/g" |
+sed -r "s/fifo_start\(0x1, /i2c_read\(/g" |
+sed -r "s/fifo_start\(0x5, /i2c_read_mot\(/g" |
+sed -r "s/fifo_start\(0x0, /i2c_write\(/g" |
+sed -r "s/fifo_start\(0x4, /i2c_write_mot\(/g" |
+
+# Prune delay-wait loops
+sed -r "s/\r\tget_status[^\r]*\r\tradeon_delay[^\r]*//g" |
+# Remove repetitive get_status calls. Those are wait loops.
+sed -r "s/(\r\tget_status[^\r]*)*/\1/g" |
+
+# This sequence indicates we're reading the reply field
+sed -r "s/fifo_switch[^\r]*\r\tfifo_read/get_reply/g" |
+
+tr '\r' '\n' |
+
+# Remaining writes to FIFO CTL are just leftovers of a mask operation
+sed -r "/radeon_write\($fifo_ctl/d" |
+
+# Remove the following line if you need to see status reads
+sed -r "/get_status\(/d" |
+sed -r "/get_reply[^$hex]*00/d"
