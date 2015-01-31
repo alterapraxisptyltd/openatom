59d72f944e2011a30d32b5c12f78e514c151ea7d
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2015-01-31 15:37:36 -0600

    coreboot_glue.h: add definition of lb_framebuffer

--- radeon_replay/coreboot_glue.h
+++ radeon_replay/coreboot_glue.h
@@ -15,6 +15,25 @@ struct i2c_seg
 	int len;
 };
 
+struct lb_framebuffer {
+	uint32_t tag;
+	uint32_t size;
+
+	uint64_t physical_address;
+	uint32_t x_resolution;
+	uint32_t y_resolution;
+	uint32_t bytes_per_line;
+	uint8_t bits_per_pixel;
+	uint8_t red_mask_pos;
+	uint8_t red_mask_size;
+	uint8_t green_mask_pos;
+	uint8_t green_mask_size;
+	uint8_t blue_mask_pos;
+	uint8_t blue_mask_size;
+	uint8_t reserved_mask_pos;
+	uint8_t reserved_mask_size;
+};
+
 void udelay(unsigned usecs);
 void mdelay(unsigned msecs);
 void delay(unsigned secs);
