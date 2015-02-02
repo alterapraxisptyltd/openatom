ed47516a5f10300b3cfe56c0aff6e96093679874
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2015-02-01 22:25:25 -0600

    radeon_replay.c stash pop

--- radeon_replay/radeon_replay.c
+++ radeon_replay/radeon_replay.c
@@ -15,13 +15,15 @@
 struct global_cfg {
 	bool need_io_perm;
 	bool run_replay;
+	bool run_asic_init;
+	bool run_init;
 	bool run_tests;
 	bool read_dcpd;
 	bool read_edid;
 	bool do_haxorz_stuff;
 };
 
-static void dump_array(const uint8_t *what, size_t len)
+void dump_array(const uint8_t * what, size_t len)
 {
 	size_t i;
 	for (i = 0; i < len; i++) {
@@ -35,55 +37,161 @@ static void dump_array(const uint8_t *what, size_t len)
 void aruba_transmitter_enable(struct radeon_device *rdev,
 			      struct DIG_TRANSMITTER_PARAMETERS *cfg);
 
-const int32_t trymafreq[] = {20000, 10000, 53300, 40000, 0, 0, 53300, 40000, 0, 0,
-			33489, 16744, 49656, 24828, 68572, 34286, 25000, -1};
-const int32_t trymafreq2[] = {5806, 5950, 6000, 6100, 6200, 6300, -1};
+const int32_t trymafreq[] =
+    { 200000, 100000, 533000, 400000, 0, 0, 533000, 400000, 0, 0,
+	334890, 167440, 496560, 248280, 685720, 342860, 250000, -1
+};
+const int32_t trymafreq2[] = { 5806, 5950, 6000, 6100, 6200, 6300, -1 };
 
-extern uint8_t more_compute_mem_eng_pll(uint32_t *clock);
+extern uint8_t more_compute_mem_eng_pll(uint32_t * clock);
 void aruba_brightness_control(struct radeon_device *rdev,
 			      uint16_t bl_pwm_freq_hz, uint8_t bl_level);
 extern void aruba_lcd_blon(struct radeon_device *rdev);
-extern void aruba_lcd_bloff(struct radeon_device *rdev, uint8_t max_pclk);
+extern void aruba_lcd_bloff(struct radeon_device *rdev);
+void travis_init(struct radeon_device *rdev);
+void travis_write(struct radeon_device *rdev, uint16_t reg, uint8_t dat);
+uint8_t travis_read(struct radeon_device *rdev, uint16_t reg);
+void travis_get_some(uint8_t data[2]);
+void aruba_set_crtc_dtd_timing(struct radeon_device *rdev, uint8_t crtc_id,
+			       struct drm_display_mode *mode,
+			       uint8_t h_border, uint8_t v_border);
+
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/mman.h>
+
+static struct lb_framebuffer fb_magic = {
+	.size = 0,
+	.physical_address = 0x5f000000,
+	.x_resolution = 1366,
+	.y_resolution = 768,
+	.bytes_per_line = 0,
+	.bits_per_pixel = 32,
+	.red_mask_pos = 0,
+	.red_mask_size = 8,
+	.green_mask_pos = 8,
+	.green_mask_size = 8,
+	.blue_mask_pos = 16,
+	.blue_mask_size = 8,
+	.reserved_mask_pos = 24,
+	.reserved_mask_size = 8,
+};
+
+void *map_fb(void)
+{
+	int fd;
+	void *rfbmmio;
+
+	fd = open("/dev/mem", O_RDWR | O_SYNC);
+
+	if (fd == -1) {
+		fprintf(stderr, "No /dev/mem ? %s\n", strerror(errno));
+		 NULL;
+	}
+
+	rfbmmio = mmap(0, 256*1024*1024, PROT_READ | PROT_WRITE, MAP_SHARED,
+		       fd, fb_magic.physical_address);
+
+	if (rfbmmio == MAP_FAILED) {
+		fprintf(stderr, "mmap() failed %s\n", strerror(errno));
+		return NULL;
+	}
+
+	return rfbmmio;
+}
+
+static void my_mask32(void *addr, uint32_t clr, uint32_t set)
+{
+	uint32_t mmio32;
+	volatile uint32_t *base = addr;
+	mmio32 = *base;
+	mmio32 &= ~clr;
+	mmio32 |= set;
+	*base = mmio32;
+}
+
+static uint16_t pitch(uint16_t x)
+{
+	return (x + 0x7f) & ~0x7f;
+}
+
+static size_t byte_pitch(uint16_t x)
+{
+	return (pitch(x) * fb_magic.bits_per_pixel) / 8;
+}
+
+static
+void get_pixel_offset32(uint16_t x, uint16_t y, uintptr_t *off)
+{
+	uint64_t bits = x * fb_magic.bits_per_pixel;
+
+	*off = y * byte_pitch(x);
+	*off += bits / 8;
+}
+
+static void put_pixel(void *fb, uint16_t x, uint16_t y, uint32_t color)
+{
+	uint8_t red = (color >> 16) & 0xff;
+	uint8_t grn = (color >>  8) & 0xff;
+	uint8_t blu = (color >>  0) & 0xff;
+	uint16_t rmask = (1 << fb_magic.red_mask_size) - 1;
+	uint16_t bmask = (1 << fb_magic.blue_mask_size) - 1;
+	uint16_t gmask = (1 << fb_magic.green_mask_size) - 1;
+	uintptr_t off;
+	uint32_t mask, set;
+
+	get_pixel_offset32(x, y, &off);
+
+	printf("pixel %d x %d, at off 0x%zx\n", x, y, off);
+	off += (uintptr_t)fb;
+
+	mask = (rmask << fb_magic.red_mask_pos)
+	     | (gmask << fb_magic.green_mask_pos)
+	     | (bmask << fb_magic.blue_mask_pos);
+	set = (red << fb_magic.red_mask_pos)
+	    | (blu << fb_magic.blue_mask_pos)
+	    | (grn << fb_magic.green_mask_pos);
+
+	my_mask32((void*)off, mask, set);
+
+}
 
 static void localtest(void)
 {
-	uint8_t div;
-	uint32_t i, freq;
+	size_t y;
+	void *fb = map_fb();
 
-	struct DIG_TRANSMITTER_CONTROL_PARAMETERS_V1_5 cfg = {
-		.usSymClock = 0x6978,
-		.ucPhyId = 0x04,
-		.ucAction = 0x01,
-		.ucLaneNum = 0x01,
-		.ucConnObjId = 0x0e, // CONNECTOR_OBJECT_ID_LVDS
-		.ucDigMode = 0,
-		.ucConfig = 0x1a,
-		.ucDigEncoderSel = 0x10,
-		.ucDPLaneSet = 0,
-	};
+	fb_magic.bytes_per_line = byte_pitch(fb_magic.x_resolution);
+	fb_magic.size = fb_magic.bytes_per_line * fb_magic.y_resolution;
 
-	aruba_transmitter_enable(NULL, &cfg);
-	return;
-	//aruba_lcd_bloff(NULL, 0);
-	//return;
-	aruba_lcd_blon(NULL);
-	for (i = 0; i < 256; i++) {
-		aruba_brightness_control(NULL, 200, i);
-		radeon_udelay(100000/(i + 1));
+	if (0) {
+		aruba_brightness_control(NULL, 200, 254);
+		if (1)
+			aruba_lcd_blon(NULL);
+		else
+			aruba_lcd_bloff(NULL);
 	}
-	return;
 
-	i = 0;
-	do {
-		freq = trymafreq[i];
-		div = more_compute_mem_eng_pll(&freq);
-		printf("Want %u, got %u, div %u\n", trymafreq[i], freq, div);
-	} while (trymafreq[++i] != -1);
-	return;
-	for (i = 0; i < 200000; i += 1000) {
-		freq = i;
-		div = more_compute_mem_eng_pll(&freq);
-		printf("Want %u, got %u, div %u\n", i, freq, div);
+	/*
+	 * FUS_MC_VM_FB_OFFSET -- location of RAM framebuffer in 4 MiB units
+	 * aruba_read(NULL, 0x2068);
+	 */
+	aruba_read(NULL, 0x2068);
+
+	memset(fb, 0, fb_magic.size);
+
+	put_pixel(fb, 683, 383, 0x0000ff);
+	put_pixel(fb, 683, 384, 0x00ff00);
+	put_pixel(fb, 683, 500, 0xff0000);
+
+	for (y = 0; y < libreradeon_logo.height; y++) {
+		uintptr_t line = (uintptr_t)fb;
+		line += y * fb_magic.bytes_per_line;
+		size_t cp_size = libreradeon_logo.width * 4;
+		size_t src_idx = cp_size * y;
+		const void *src = &libreradeon_logo.pixel_data[src_idx];
+		memcpy((void *)line, src, cp_size);
 	}
 }
 
@@ -92,6 +200,8 @@ static void print_help(void)
 	printf("radeon_replay [ACTIONS]\n\n");
 	printf("  -h, --help   Print this message and exit\n");
 	printf("  -r, --replay Replay a VGA init (source: hp_1035dx)\n");
+	printf("  -a, --asic_init   Run asic_init before everything else\n");
+	printf("  -i, --init   Try to initialize display and draw something\n");
 	printf("  -T, --tests  Run a random assortment of tests\n");
 	printf("  -d, --dpcd   Read DisplayPort configuration data\n");
 	printf("  -e, --edid   Read EDID over AUX channel (DANGEROUS)\n");
@@ -102,8 +212,10 @@ static void parse_options(int argc, char *argv[], struct global_cfg *config)
 {
 	int opt, option_index = 0;
 	struct option long_options[] = {
-		{"help",	no_argument, 		0, 'h'},
+		{"help",	no_argument,		0, 'h'},
 		{"replay",	no_argument,		0, 'r'},
+		{"asic-init",	no_argument,		0, 'a'},
+		{"init",	no_argument,		0, 'i'},
 		{"tests",	no_argument,		0, 'T'},
 		{"dpcd",	no_argument,		0, 'd'},
 		{"edid",	no_argument,		0, 'e'},
@@ -118,7 +230,7 @@ static void parse_options(int argc, char *argv[], struct global_cfg *config)
 	 * Parse argumentsx
 	 */
 	while (1) {
-		opt = getopt_long(argc, argv, "hrtdetX",
+		opt = getopt_long(argc, argv, "hraiTdetX",
 				  long_options, &option_index);
 
 		if (opt == EOF)
@@ -128,6 +240,10 @@ static void parse_options(int argc, char *argv[], struct global_cfg *config)
 		case 'r':
 			config->need_io_perm = true;
 			config->run_replay = true;
+			break;
+		case 'i':
+			config->need_io_perm = true;
+			config->run_init = true;
 			break;
 		case 'T':
 			config->need_io_perm = true;
@@ -161,13 +277,6 @@ static void parse_options(int argc, char *argv[], struct global_cfg *config)
 
 int main(int argc, char *argv[])
 {
-	uint32_t mmio32;
-	volatile uint32_t *base = addr;
-	mmio32 = *base;
-	mmio32 &= ~clr;
-	mmio32 |= set;
-	*base = mmio32;
-}
 
 	int dpcd_ret, edid_ret;
 	uint8_t dpcd[256], edid_raw[256];
@@ -176,6 +285,7 @@ int main(int argc, char *argv[])
 	printf("radeon_replay AtomBIOS replayer\n");
 
 	parse_options(argc, argv, &config);
+	radeon_init_mmio();
 
 	if (config.need_io_perm) {
 		if (ioperm(0x2000, 0x2000, 1) || ioperm(0x300, 0x100, 1)) {
@@ -208,7 +318,7 @@ int main(int argc, char *argv[])
 		run_radeon_tests();
 
 	if (config.run_replay) {
-		asic_init();
+		///aruba_asic_init(NULL);
 		printf("Replaying initial init\n");
 		run_replay(1);
 
@@ -227,6 +337,9 @@ int main(int argc, char *argv[])
 	if (config.do_haxorz_stuff) {
 		localtest();
 	}
+
+	if (config.run_init)
+		execute_master_plan(NULL);
 
 	return EXIT_SUCCESS;
 }
